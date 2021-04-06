8e2eb01ec1891aa176dd32e176bc247b69ca6651
yerssin rios <yerssin15@gmail.com>
2021-04-06 10:35:40 -0400

    integracion a componentes

--- src/Semaforo.js
+++ src/Semaforo.js
@@ -1,33 +1,43 @@
 import React from "react";
 import './Semaforo.css';
+import { useState } from "react";
 
-export default class Semaforo extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      clickColor: null
-    };
+function Semaforo() {
+
+  const [colorSeleccionadoRojo, setColorSeleccionadoRojo] = useState('');
+  const [colorSeleccionadoAmarillo, setColorSeleccionadoAmarillo] = useState('');
+  const [colorSeleccionadoVerde, setColorSeleccionadoVerde] = useState('');
+
+  function setColorSeleccionado(e) {
+    if (e.target.classList.contains("rojo")) {
+      setColorSeleccionadoRojo("selected");
+      setColorSeleccionadoAmarillo("");
+      setColorSeleccionadoVerde("");
+    }
+
+    if (e.target.classList.contains("amarillo")) {
+      setColorSeleccionadoRojo("");
+      setColorSeleccionadoAmarillo("selected");
+      setColorSeleccionadoVerde("");
+    }
+
+    if (e.target.classList.contains("verde")) {
+      setColorSeleccionadoRojo("");
+      setColorSeleccionadoAmarillo("");
+      setColorSeleccionadoVerde("selected");
+    }
   }
-
-  render() {
-    console.log(this.state);
-
-    let rojoClassAditional = "";
-    if (this.state.clickColor === "rojo") rojoClassAditional = "selected";
-
-    let amarilloClassAditional = "";
-    if (this.state.clickColor === "amarillo") amarilloClassAditional = "selected";
-
-    let verdeClassAditional = "";
-    if (this.state.clickColor === "verde") verdeClassAditional = "selected";
-
-    return <div id="padre">
+  return (
+    <div id="padre">
       <div id="palo"></div>
       <div className="caja">
-        <div className={"rojo luz " + rojoClassAditional} onClick={() => this.setState({ clickColor: "rojo" })}></div>
-        <div className={"amarillo luz " + amarilloClassAditional} onClick={() => this.setState({ clickColor: "amarillo" })}></div>
-        <div className={"verde luz " + verdeClassAditional} onClick={() => this.setState({ clickColor: "verde" })}></div>
+        <div className={"rojo luz " + colorSeleccionadoRojo} onClick={setColorSeleccionado}></div>
+        <div className={"amarillo luz " + colorSeleccionadoAmarillo} onClick={setColorSeleccionado}></div>
+        <div className={"verde luz " + colorSeleccionadoVerde} onClick={setColorSeleccionado}></div>
       </div>
-    </div>;
-  }
+    </div>
+
+  );
 }
+
+export default Semaforo
